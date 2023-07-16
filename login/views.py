c1720d088990f215e3bb6c46008e796592e27c10
MeongGanas <farreluken@gmail.com>
2023-07-16 16:43:13 +0800

    membuat sistem login

--- login/views.py
+++ login/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.contrib.auth import authenticate, login
+from .forms import FormLogin
 
 
 def login_view(request):
+    form = FormLogin()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/login/dashboard')
+    return render(request, 'login_view.html', {'form': form})
+
+
+def dashboard(request):
     context = {
-        "title": "Login"
+        "title": "Dashboard"
     }
-    return render(request, 'login_view.html', context)
+    return render(request, 'dashboard.html', context)
