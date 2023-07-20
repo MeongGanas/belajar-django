160116db0ac19ebd35472e93502dec2a0a865ea5
MeongGanas <farreluken@gmail.com>
2023-07-20 19:43:13 +0800

    make model dan list display

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from dashboard.models import Anggota
 # Register your models here.
+
+
+@admin.register(Anggota)
+class anggotaAdmin(admin.ModelAdmin):
+    list_display = ['nama', 'umur', 'jenis_kelamin', 'alamat']
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Anggota(models.Model):
+    nama = models.CharField(max_length=225)
+    alamat = models.CharField(max_length=225)
+    umur = models.IntegerField()
+
+    JENIS_KELAMIN = (
+        ('laki-laki', 'laki-laki'),
+        ('perempuan', 'perempuan')
+    )
+    jenis_kelamin = models.CharField(max_length=225, choices=JENIS_KELAMIN)
